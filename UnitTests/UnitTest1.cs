c2d04016a8349ce731ed0b7e6196f079e4b711dd
lindsayalexanderTSI <lindsay.alexander@translationalsoftware.com>
2019-12-18 09:19:55 -0800

    delete .vs file

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,15 +1,25 @@
 using Console;
 using NUnit.Framework;
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace UnitTests
 {
     public class Tests
     {
+        public TestToolbox Toolbox { get; set; }
+
+        public Tests()
+        {
+
+        }
         [SetUp]
         public void Setup()
         {
+            Toolbox = new TestToolbox();
         }
 
         [Test]
@@ -17,30 +27,27 @@ namespace UnitTests
         {
             var stringToReverse = "sdlkfja;osidfulkjer";
             var sr = new StringReverser(stringToReverse);
-            TestToolbox toolbox = new TestToolbox();
-            toolbox.StringReverserStopWatch(sr, "MyStringReverser", out TimeSpan myTime);
-            toolbox.StringReverserStopWatch(sr, "TheirStringReverser", out TimeSpan theirTime);
-            Assert.IsTrue(myTime < theirTime);
-        }
 
-    }
+            int runCount = 10;
 
-    public class TestToolbox
-    {
-        public Stopwatch Watch { get; set; }
+            TimeSpan theirTimeTotal = new TimeSpan();
+            TimeSpan myTimeTotal = new TimeSpan();
 
+            for (int i = 0; i < runCount; i++)
+            {
 
-        public TestToolbox()
-        {
-            Watch = new Stopwatch();
-        }
-        public void StringReverserStopWatch(StringReverser sr, string methodName, out TimeSpan time)
-        {
-            Watch.Reset();
-            Watch.Start();
-            typeof(StringReverser).GetMethod(methodName).Invoke(sr, null);
-            Watch.Stop();
-            time = Watch.Elapsed;
+                Toolbox.StringReverserStopWatch(sr, "MyStringReverser", out TimeSpan myTime);
+                myTimeTotal += myTime;
+                Toolbox.StringReverserStopWatch(sr, "TheirStringReverser", out TimeSpan theirTime);
+                theirTimeTotal += theirTime;
+            }
+
+            TimeSpan myaverage = myTimeTotal / runCount;
+            TimeSpan theirAverage = theirTimeTotal / runCount;
+            MarkdownLog log = new MarkdownLog();
+            KeyValuePair<string, string> mine = new KeyValuePair<string, string>("My Average Time", Math.Round(myaverage.TotalMilliseconds, 4).ToString());
+            KeyValuePair<string, string> theirs = new KeyValuePair<string, string>("Their Average Time", Math.Round(theirAverage.TotalMilliseconds, 4).ToString());
+            log.LogAsMarkdownTable(new KeyValuePair<string, string>[] { mine, theirs });
         }
     }
 }
