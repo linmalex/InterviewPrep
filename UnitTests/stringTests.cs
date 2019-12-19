a0f7f5a807fbe172617232390ed77cefe0dfbe55
lindsayalexanderTSI <lindsay.alexander@translationalsoftware.com>
2019-12-19 14:39:41 -0800

    add palindrome test

--- UnitTests/stringTests.cs
+++ UnitTests/stringTests.cs
@@ -23,5 +23,19 @@ namespace UnitTests
         {
         }
 
+        [Test]
+        public void PalindromeTest()
+        {
+            DirectoryInfo solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent;
+            var unitTestFilesDir = Path.Combine(solutionDirectory.FullName, @"UnitTests\TestFiles");
+            var palindromesFile = Path.Combine(unitTestFilesDir, "palindromes.json");
+
+            JObject jObj = JObject.Parse(File.ReadAllText(palindromesFile));
+            IEnumerable<bool> trueItems = jObj["true"].Children().Select(c => PalindromeTool.MyPalindromeChecker(c.ToString()));
+            IEnumerable<bool> falseItems = jObj["false"].Children().Select(c => PalindromeTool.MyPalindromeChecker(c.ToString()));
+
+            Assert.False(trueItems.Contains(false));
+            Assert.False(falseItems.Contains(true));
+        }
     }
 }
