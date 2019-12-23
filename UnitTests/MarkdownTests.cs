957177c3609f05b5d380f054c76dfcf35a2978bd
Lindsay Alexander <linmalex@gmail.com>
2019-12-23 12:59:49 -0800

    uncomment MarkdownLogTool

--- UnitTests/MarkdownTests.cs
+++ UnitTests/MarkdownTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using ConsoleApp;
 using System.Collections.Generic;
+using Toolbox.Models;
 
 namespace Tests
 {
@@ -10,21 +11,20 @@ namespace Tests
         public void Setup()
         {
         }
-        //todo add markdown logs back in
-        //[Test]
-        //public void MarkdownHeaderTest()
-        //{
-        //    var log = new MarkdownLog();
-        //    string[] headers = new string[] { "Name", "Age", "Another One" };
-        //    string[] headers2 = new string[] { "Name", "Age", "Another One","Adding More","To Keep it interesting" };
-        //    List<string> markdownLines = new List<string>();
-        //    List<string> markdownLines2 = new List<string>();
-        //    log.MarkdownTableHeader(headers, markdownLines);
-        //    log.MarkdownTableHeader(headers2, markdownLines2);
-        //    Assert.True(markdownLines[0] == "| Name | Age | Another One |");
-        //    Assert.True(markdownLines2[0] == "| Name | Age | Another One | Adding More | To Keep it interesting |");
-        //    Assert.True(markdownLines[1] == "| --- | --- | --- |");
-        //    Assert.True(markdownLines2[1] == "| --- | --- | --- | --- | --- |");
-        //}
+        [Test]
+        public void MarkdownHeaderTest()
+        {
+            var log = new LogTool();
+            string[] headers = new string[] { "Name", "Age", "Another One" };
+            string[] headers2 = new string[] { "Name", "Age", "Another One", "Adding More", "To Keep it interesting" };
+            List<string> markdownLines = new List<string>();
+            List<string> markdownLines2 = new List<string>();
+            log.MarkdownTableHeader(headers, markdownLines);
+            log.MarkdownTableHeader(headers2, markdownLines2);
+            Assert.True(markdownLines[0] == "| Name | Age | Another One |");
+            Assert.True(markdownLines2[0] == "| Name | Age | Another One | Adding More | To Keep it interesting |");
+            Assert.True(markdownLines[1] == "| --- | --- | --- |");
+            Assert.True(markdownLines2[1] == "| --- | --- | --- | --- | --- |");
+        }
     }
 }
